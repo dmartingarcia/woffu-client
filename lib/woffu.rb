f6cbeaee5fcbb24bdbae843301e7602cc7fc6a29
David Martin Garcia <david.martin@returnly.com>
2022-04-25 22:29:27 +0200

    initialize gem with bundle gem command

---
+++ lib/woffu.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "woffu/version"
+
+module Woffu
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/woffu/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Woffu
+  VERSION = "0.1.0"
+end
