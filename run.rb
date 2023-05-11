731a86e6f6935a2607b90b6ab46c94948a2cbe5c
David Martin Garcia <david.martingarcia@affirm.com>
2023-05-11 16:38:03 -0700

    using a vriable for authorization_headers and fixing env var checking

--- run.rb
+++ run.rb
@@ -10,8 +10,8 @@ end
 pass = ENV['WOFFU_PASSWORD']
 user = ENV['WOFFU_EMAIL'] 
 
-user = get_user_input_for("woffu email") if user.nil? || user.empty?
-pass = get_user_input_for("password") if user.nil? || pass.empty?
+user = get_user_input_for("woffu email") if user.nil? || user&.empty?
+pass = get_user_input_for("password") if pass.nil? || pass&.empty?
 
 login_response = Typhoeus.get('https://app.woffu.com/token', body: {grant_type: :password, username: user, password: pass})
 
@@ -20,15 +20,18 @@ raise "Unsuccessful login" if login_response.response_code != 200
 token = JSON.parse(login_response.body)['access_token']
 bearer_token = "Bearer #{token}"
 
-company_data = Typhoeus.get('https://app.woffu.com/api/companies/companiesbyemail', headers: {authorization: bearer_token})
+authorization_headers = { authorization: bearer_token }
+
+company_data = Typhoeus.get('https://app.woffu.com/api/companies/companiesbyemail', headers: authorization_headers)
 company_data = JSON.parse(company_data.body)
+
 company_id = company_data.first["CompanyId"]
 
 #post
-remember_me = Typhoeus.post('https://app.woffu.com/api/users/rememberme', headers: {authorization: bearer_token})
+remember_me = Typhoeus.post('https://app.woffu.com/api/users/rememberme', headers: authorization_headers)
 remember_me = JSON.parse(remember_me.body)
 
-users = Typhoeus.get('https://app.woffu.com/api/users', headers: {authorization: bearer_token})
+users = Typhoeus.get('https://app.woffu.com/api/users', headers: authorization_headers)
 users = JSON.parse(users.body)
 user_id = users["UserId"]
 calendar_id = users["Calendar"]["CalendarId"]
@@ -38,43 +41,41 @@ calendar_id = users["CalendarId"]
 schedule_id = users["ScheduleId"]
 break_time = 1 #TrueBreakHours
 
-companies = Typhoeus.get("https://app.woffu.com/api/companies/#{company_id}", headers: {authorization: bearer_token})
+companies = Typhoeus.get("https://app.woffu.com/api/companies/#{company_id}", headers: authorization_headers)
 companies = JSON.parse(companies.body)
+
+api_domain = companies["Domain"]
 company_id = company_data.first["CompanyId"]
 subscription_id = companies["Subscription"]["SubscriptionId"]
 
-company_settings = Typhoeus.get("https://app.woffu.com/api/companies/#{company_id}/settings", headers: {authorization: bearer_token})
+company_settings = Typhoeus.get("https://app.woffu.com/api/companies/#{company_id}/settings", headers: authorization_headers)
 company_settings = JSON.parse(company_settings.body)
 
-feature_flags = Typhoeus.get("https://app.woffu.com/api/companies/#{company_id}/feature-flags-data", headers: {authorization: bearer_token})
+feature_flags = Typhoeus.get("https://app.woffu.com/api/companies/#{company_id}/feature-flags-data", headers: authorization_headers)
 feature_flags = JSON.parse(feature_flags.body)
 
-subscription_details = Typhoeus.get("https://app.woffu.com/api/svc/subscriptions/subscription/#{subscription_id}/basic", headers: {authorization: bearer_token})
+subscription_details = Typhoeus.get("https://app.woffu.com/api/svc/subscriptions/subscription/#{subscription_id}/basic", headers: authorization_headers)
 subscription_details = JSON.parse(subscription_details.body)
 
-personal_setup = Typhoeus.get("https://app.woffu.com/api/svc/dashboard/personal/setup", headers: {authorization: bearer_token})
+personal_setup = Typhoeus.get("https://app.woffu.com/api/svc/dashboard/personal/setup", headers: authorization_headers)
 personal_setup = JSON.parse(personal_setup.body)
 
-workday_lite = Typhoeus.get("https://returnly.woffu.com/api/users/#{user_id}/workdaylite", headers: {authorization: bearer_token})
+workday_lite = Typhoeus.get("https://#{api_domain}/api/users/#{user_id}/workdaylite", headers: authorization_headers)
 workday_lite = JSON.parse(workday_lite.body)
 
 time_spans = workday_lite["TimeSpans"].map{|e| [e["StartTime"], e["EndTime"]]}
 
-# Nice user tracking that sends deviceid coordinates and some tracking shit, thanks woffu for my privacy
-#signs = Typhoeus.get("https://returnly.woffu.com/api/svc/signs/signs", headers: {authorization: bearer_token}, body: {tracking_shit})
-#signs = JSON.parse(signs.body)
-
 date_yesterday = (Date.today - 1).to_s
 date_60_days_ago = (Date.today - 60).to_s
 
-diaries_presence = Typhoeus.get("https://returnly.woffu.com/api/users/#{user_id}/diaries/presence?fromDate=#{date_60_days_ago}&pageIndex=0&pageSize=100&toDate=#{date_yesterday}", headers: {authorization: bearer_token})
+diaries_presence = Typhoeus.get("https://#{api_domain}/api/users/#{user_id}/diaries/presence?fromDate=#{date_60_days_ago}&pageIndex=0&pageSize=100&toDate=#{date_yesterday}", headers: authorization_headers)
 diaries = JSON.parse(diaries_presence.body)["Diaries"]
 
 diaries_details = diaries.map{|e| {id: e["DiaryId"], date: e["Date"], hours_to_work: e["HoursToWork"], hours_worked: e["HoursWorked"], true_schedule: e["TrueScheduleHours"], not_categorised: e["HoursNoCategorized"]}}
 
 
 def get_diary_slots(bearer_token: nil, diary_id: nil)
-  diary_slot = Typhoeus.get("https://returnly.woffu.com/api/diaries/#{diary_id}/workday/slots/self", headers: {authorization: bearer_token})
+  diary_slot = Typhoeus.get("https://#{api_domain}/api/diaries/#{diary_id}/workday/slots/self", headers: authorization_headers)
   JSON.parse(diary_slot.body)["Slots"]
 end
 
@@ -140,8 +141,8 @@ def put_diary_slots(bearer_token: nil, diary_id: nil, user_id: nil, start: nil, 
       generate_diary_slot(sign_id: sign_id, user_id: user_id, date: date, time_start: break_stop, time_stop: stop, order: 2, total_slot: 3),
     ]
   }
-    
-  diary_slot = Typhoeus.put("https://returnly.woffu.com/api/diaries/#{diary_id}/workday/slots/self", headers: {authorization: bearer_token, "content-type" => "application/json;charset=UTF-8"}, body: body.to_json)
+
+  diary_slot = Typhoeus.put("https://#{api_domain}/api/diaries/#{diary_id}/workday/slots/self", headers: authorization_headers_with_content_type), body: body.to_json)
   raise "Error!! Date: #{date}" if diary_slot.response_code != 204
 end
 
@@ -154,6 +155,27 @@ diaries_invalid.each do |invalid_diary|
   puts "Diary #{invalid_diary[:date]} will be overriden. [y/n]"
   input = gets
   next if input.chomp != "y"
-  
-  put_diary_slots(bearer_token: bearer_token, diary_id: invalid_diary[:id], user_id: user_id, start: "09:00:00", break_start: "14:00:00", break_stop: "15:00:00", stop: "18:00:00", department_id: department_id, job_title_id: job_title_id, calendar_id: calendar_id, schedule_id: schedule_id, break_time: break_time, date: invalid_diary[:date].split("T").first, sign_id: 0)
+
+  put_diary_slots(bearer_token: bearer_token, diary_id: invalid_diary[:id], user_id: user_id, start: "09:00:00", break_start: "14:00:00", break_stop: "15:00:00", stop: "18:00:00", department_id: department_id, job_title_id: job_title_id, calendar_id: calendar_id, schedule_id: schedule_id, break_time: break_time, date: invalid_diary[:date].split("T").first, sign_id: 0, authorization_headers_with_content_type)
 end
+
+def sign_in(value: false, user_id: nil, api_domain: nil, authorization_headers)
+
+  body = {
+    "AgreementEventId" => nil,
+    "DeviceId" => "WebApp",
+    "EndDate" => "2023-05-11T09:22:18+02:00",
+    "Latitude" => nil,
+    "Longitude" => nil,
+    "RequestId" => nil,
+    "ShortTrueTime" => "09:22:18",
+    "StartDate" => "2023-05-11T09:22:18+02:00",
+    "TimezoneOffset" => "120",
+    "UserId" => user_id,
+    "signIn" => value
+  }
+
+  Typhoeus.post("https://#{api_domain}/api/svc/signs/signs", body: body, headers: authorization_headers)
+end
+
+sign_in(value: true, user_id: user_id, api_domain: api_domain, authorization_headers: authorization_headers_with_content_type)
